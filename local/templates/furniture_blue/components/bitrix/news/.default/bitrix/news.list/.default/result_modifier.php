beacdf51d7d364ca1ca1fcfb04253eaf24cdfbc1
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2021-03-01 11:58:00 +0300

    solution of the task

---
+++ local/templates/furniture_blue/components/bitrix/news/.default/bitrix/news.list/.default/result_modifier.php
@@ -0,0 +1,8 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+if($arParams["SPECIALDATE"] == "Y")
+{
+    $firstDateNews = $arResult["ITEMS"][0]["ACTIVE_FROM"];
+    $arResult["SPECIALDATE"] = $firstDateNews;
+    $this->getComponent()->SetResultCacheKeys(array("SPECIALDATE"));
+}?>
